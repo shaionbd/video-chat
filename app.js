87a14685dedbd48eb360b55a38486a608366ad26
shaionbd <shakil.shaion@gmail.com>
2018-01-03 16:01:04 +0600

    template with MVC pattern

--- app.js
+++ app.js
@@ -1,28 +1,17 @@
 var express = require('express');
-var bodyParser = require('body-parser');
+// controllers
+var userController = require('./controllers/userController');
 
+// execute express framework
 var app = express();
-
-// create application/x-www-form-urlencoded parser
-var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 // set up template engine
 app.set('view engine', 'ejs');
 // middleware access for assets folder
 app.use('/assets', express.static('assets'));
 
-// routing pages
-app.get('/login', function(req, res){
-	res.render('login');
-});
-
-// POST /login gets urlencoded bodies
-app.post('/login', urlencodedParser, function (req, res) {
-  if (!req.body) return res.sendStatus(400)
-  // check datbase
-  // go back to login page if username & password are not matched
-  // go to dashboard if login successfully
-  
-})
+// fire controllers
+userController(app);
 
 app.listen(3000);
+console.log('Server is running at 3000 port');
