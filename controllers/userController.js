f5e80dbe1812026de565f19b9d4deb628d7f31e5
shaionbd <shakil.shaion@gmail.com>
2018-01-04 13:07:15 +0600

    login page connet with database

--- controllers/userController.js
+++ controllers/userController.js
@@ -13,29 +13,60 @@ var userSchema = new mongoose.Schema({
 });
 
 var User = mongoose.model('User', userSchema);
-// var user1 = User({email: 'admin@gmail.com', name: 'Admin', pass: '12345', chat_id: '0987654321'}).save(function(err){
-// 	if(err) throw err;
-// 	console.log('User saved');
-// });
+
+var userSessionData = {};
 
 // create application/x-www-form-urlencoded parser
 var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 module.exports = function(app){
 	// routing pages
+	// 
+	app.get('/', function(req, res){
+		User.find({email: 'admin@gmail.com', pass: '123456'}, function(err, data){
+		  	if(err) throw err;
+		  	if(data.length == 0){
+		  		console.log('No user found!');
+		  	}
+		  	console.log(data);
+		  	res.render('dashboard', {data: data});
+	  	}); 
+	});
 
 	// login route
 	app.get('/login', function(req, res){
-		res.render('login');
+
+		res.render('login', {error: false});
 	});
 	app.post('/login', urlencodedParser, function (req, res) {
-	  if (!req.body) return res.sendStatus(400)
+	  if (!req.body) return res.sendStatus(400);
 	  // check datbase
-	  // go back to login page if username & password are not matched
-	  // go to dashboard if login successfully  
-	});
-	app.delete('/login', function(req, res){
+	  User.find({email: req.body.email, pass: req.body.pass}, function(err, data){
+	  	if(err) throw err;
 
+	  	if(data.length == 0){
+	  		res.render('login', {error: true});
+	  	}else{
+	  		// save user info on session 
+	  		res.render('dashboard');
+	  	}
+	  	
+	  }); 
+	});
+	app.post('/add/user', urlencodedParser, function(req, res){
+		if (!req.body) return res.sendStatus(400);
+		var user1 = User({email: req.body.email, name: req.body.name, pass: req.body.pass, chat_id: req.body.chat_id}).save(function(err){
+			if(err) throw err;
+			console.log('User saved');
+			res.json(req.body);
+		});
+	});
+
+	app.delete('/delete/user/:email', function(req, res){
+		User.find({email: req.params.email}).remove(function(err, data){
+			if(err) throw err;
+			res.json(data);
+		});
 	});
 
 	// 
