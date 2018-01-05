f0ca7a751ea7da9a3cf3a237110068f81162c328
Shakil Hossain <shakil.shaion@gmail.com>
2018-01-05 12:28:27 +0600

    dashboard view added

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,9 @@
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
+var session = require('express-session');
+
+
+
 
 // connect to database
 mongoose.connect('mongodb://shaion:fs0c!3ty@ds239557.mlab.com:39557/video_chat');
@@ -14,27 +18,38 @@ var userSchema = new mongoose.Schema({
 
 var User = mongoose.model('User', userSchema);
 
-var userSessionData = {};
-
 // create application/x-www-form-urlencoded parser
 var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 module.exports = function(app){
+ 
+	app.use(session({ secret: 'decima technology', resave: false, saveUninitialized: false, cookie: { maxAge: 6000000 }}));
+
+
 	// routing pages
 	// 
 	app.get('/', function(req, res){
-		User.find({email: 'admin@gmail.com', pass: '123456'}, function(err, data){
+		
+		User.find({}, function(err, data){
 		  	if(err) throw err;
-		  	if(data.length == 0){
-		  		console.log('No user found!');
-		  	}
-		  	console.log(data);
-		  	res.render('dashboard', {data: data});
-	  	}); 
+		  	console.log("Users: "+data);
+		  	res.render('dashboard', {users: data});
+		});
+
+		//if(req.session.login){
+			
+		//}else{
+			//res.redirect('/login');
+		//}
+		
 	});
 
 	// login route
 	app.get('/login', function(req, res){
+		if(req.session.login){
+			res.redirect('/');
+		}
+		console.log('login page');
 
 		res.render('login', {error: false});
 	});
@@ -47,25 +62,49 @@ module.exports = function(app){
 	  	if(data.length == 0){
 	  		res.render('login', {error: true});
 	  	}else{
-	  		// save user info on session 
-	  		res.render('dashboard');
+	  		// save user info on session
+	  		console.log(data);
+	  		req.session.login = true;
+	  		req.session.email = data[0].email;
+	  		req.session.name = data[0].name;
+	  		req.session.chat_id = data[0].chat_id;
+	  		res.redirect('/');
 	  	}
 	  	
 	  }); 
 	});
 	app.post('/add/user', urlencodedParser, function(req, res){
 		if (!req.body) return res.sendStatus(400);
-		var user1 = User({email: req.body.email, name: req.body.name, pass: req.body.pass, chat_id: req.body.chat_id}).save(function(err){
+		var user1 = User({email: req.body.email, name: req.body.name, pass: req.body.pass, chat_id: 1233233453}).save(function(err){
 			if(err) throw err;
 			console.log('User saved');
-			res.json(req.body);
+			res.redirect('/');
+			//res.json(req.body);
 		});
 	});
 
-	app.delete('/delete/user/:email', function(req, res){
-		User.find({email: req.params.email}).remove(function(err, data){
+	app.get('/delete/user/:id', function(req, res){
+		User.find({_id: req.params.id}).remove(function(err, data){
 			if(err) throw err;
-			res.json(data);
+			console.log('deleted');
+			res.redirect('/');
+			//res.json(data);
+		});
+	});
+
+	// app.delete('/delete/user/:id', function(req, res){
+	// 	User.find({id: req.params.id}).remove(function(err, data){
+	// 		if(err) throw err;
+	// 		res.redirect('/');
+	// 		//res.json(data);
+	// 	});
+	// });
+
+	app.get('/logout', function(req, res){
+		req.session.destroy(function(err) {
+		  // cannot access session here
+		  if(err) throw err;
+		  res.redirect('/login');
 		});
 	});
 
